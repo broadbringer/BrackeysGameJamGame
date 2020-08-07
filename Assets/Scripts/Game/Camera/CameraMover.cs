e7d236cd8d6efc947ae9fecd08f00bc355c65a57
tentacle_dream <vlad_17400@mail.ru>
2020-08-08 03:46:47 +0600

    A lot of updates. Check discord

--- Assets/Scripts/Game/Camera/CameraMover.cs
+++ Assets/Scripts/Game/Camera/CameraMover.cs
@@ -7,6 +7,53 @@ public class CameraMover : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private Transform _shopPosition;
     [SerializeField] private Transform _garagePosition;
+    private bool isMoving = false;
+    private Vector3 destination;
+    [SerializeField] private float camSpeed = 3f;
+    private Vector3 velocity = Vector3.zero;
+
+    public void Start()
+    {
+        velocity.z = -10;
+    }
+
+    public void MoveToShop()
+    {
+        isMoving = true;
+        destination = _shopPosition.position;
+    }
+
+    public void MoveToGarage()
+    {
+        isMoving = true;
+        destination = _garagePosition.position;
+    }
+
+    private void FixedUpdate()
+    {
+
+
+
+
+        if (isMoving)
+        {
+            //_camera.transform.position = Vector3.MoveTowards(transform.position, destination, camSpeed * Time.deltaTime);
+            _camera.transform.position = Vector3.SmoothDamp(_camera.transform.position, destination, ref velocity, camSpeed);
+            if (Vector3.Distance(_camera.transform.position, destination) <= 0.01f)
+            {
+                _camera.transform.position = destination;
+                isMoving = false;
+            }
+        }
+    }
+
+
+
+
+    /*
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _shopPosition;
+    [SerializeField] private Transform _garagePosition;
     
     
     public void MoveToShop()
@@ -18,4 +65,5 @@ public class CameraMover : MonoBehaviour
     {
         _camera.transform.position = _garagePosition.position;
     }
+    */
 }
